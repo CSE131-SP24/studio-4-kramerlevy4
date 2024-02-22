5add6de2490fc7f0c4b9731c783daa1630ab5fbc
nlevy <nlevy@NoahL_Surface.dhcp.wustl.edu>
2024-02-22 12:42:15 -0600

    finsihed

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,18 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenColor(0, 0, 0);
+		StdDraw.filledRectangle(0.0, 0.0, 0.2, 0.2);
+		StdDraw.filledRectangle(0.4, 0.4, 0.2, 0.2);
+		StdDraw.filledRectangle(0.8, 0.8, 0.2, 0.2);
+		
+		double[] array1 = {0.2,0.6,0.8};
+		double[] array2= {0.0,0.2,0.6};
+		StdDraw.filledPolygon(array1, array2);
+		
+		double[] array3 = {0.0,0.2,0.6};
+		double[] array4= {0.2,0.6,0.8};
+		StdDraw.filledPolygon(array3, array4);
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -8,7 +9,7 @@ import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
 
 import edu.princeton.cs.introcs.StdDraw;
-
+import edu.princeton.cs.introcs.*;
 /**
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
@@ -19,6 +20,45 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shape=in.next();
+		
+		if(shape.equals("ellipse")) {
+			int red=in.nextInt();
+			int green=in.nextInt();
+			int blue=in.nextInt();
+			boolean filled=in.nextBoolean();
+			double d1=in.nextDouble();
+			double d2=in.nextDouble();
+			double d3=in.nextDouble();
+			double d4=in.nextDouble();
+			
+			StdDraw.setPenColor(red, green, blue);
+			StdDraw.filledEllipse(d1, d2, d3, d4);
+		}
+		
+		else if(shape.equals("triangle")){
+			int red=in.nextInt();
+			int green=in.nextInt();
+			int blue=in.nextInt();
+			boolean filled=in.nextBoolean();
+			double d1=in.nextDouble();
+			double d2=in.nextDouble();
+			double d3=in.nextDouble();
+			double d4=in.nextDouble();
+			double d5=in.nextDouble();
+			double d6=in.nextDouble();
+			
+			double[] array1 = {d1,d2,d3};
+			double[] array2= {d4,d5,d6};
+					
+			
+			StdDraw.setPenColor(red, green, blue);
+			StdDraw.polygon(array1, array2);
+		}
+		
+		//StdDraw.ellipse(double x, double y, double semiMajorAxis, double semiMinorAxis)
+		//StdDraw.square(double x, double y, double halfLength)
+		//StdDraw.rectangle(double x, double y, double halfWidth, double halfHeight)
 		
 	}
 }
